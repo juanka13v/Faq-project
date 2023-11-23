e73e4fbdea90e52ae81fd66cc60eee8a2b204c53
juanka13v <juanka13v@users.noreply.github.com>
2023-11-23 02:35:30 -0300

    config ejs views

--- src/app.js
+++ src/app.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
+const path = require("path")
+const expressLayout = require("express-ejs-layouts")
 const { MONGO_URI, PORT } = require("./config");
-const connectDB = require("./db/connect")
+const connectDB = require("./db/connect");
 
+// parse json
 app.use(express.json());
 
+// config ejs
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayout)
+app.set('layout', 'layout');
+
+
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("pages/index")
+});
+
+app.get("/docs", (req, res) => {
+  res.render("pages/docs")
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/about")
 });
 
 const start = async () => {
