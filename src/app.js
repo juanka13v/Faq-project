f742ee7e5bcd79552ae9d76ecc269feba7b35f69
juanka13v <juanka13v@users.noreply.github.com>
2023-11-27 08:47:46 -0300

    feat(server): add errorHandler

--- src/app.js
+++ src/app.js
@@ -1,32 +1,37 @@
 const express = require("express");
 const app = express();
-const path = require("path")
-const expressLayout = require("express-ejs-layouts")
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 const { MONGO_URI, PORT } = require("./config");
 const connectDB = require("./db/connect");
+
+const adminRoute = require("./routes/question.admin.route");
+const errorHandler = require("./middlewares/error.middleware");
 
 // parse json
 app.use(express.json());
 
 // config ejs
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
-app.use(expressLayout)
-app.set('layout', 'layout');
+app.set("views", path.join(__dirname, "views"));
+app.use(expressLayout);
+app.set("layout", "layout");
 
-
+app.use("/admin", adminRoute);
 
 app.get("/", (req, res) => {
-  res.render("pages/index")
+  res.render("pages/index", { title: "Home" });
 });
 
 app.get("/docs", (req, res) => {
-  res.render("pages/docs")
+  res.render("pages/docs");
 });
 
 app.get("/about", (req, res) => {
-  res.render("pages/about")
+  res.render("pages/about");
 });
+
+app.use(errorHandler);
 
 const start = async () => {
   try {
